31945686deef32eda9d00acf5d9e4ac853166649
Jeep <jp@wirespeak.com>
2011-02-26 14:10:49 -0500

    Adding images upload form

---
+++ site/index.php
@@ -0,0 +1,28 @@
+<?php
+  require 'pixiClass.php';
+  require_once 'showImages.php';
+  require_once 'imageSplitter.php';
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <link rel = "stylesheet" type="text/css" href="style.css" >
+  <title>hackOTT</title>
+  <script type="text/javascript" charset="utf-8" src="jquery.js"></script>
+  <script type="text/javascript" charset="utf-8" src="scripts.js"></script>
+</head>
+<body>
+<div id="page-wrapper">
+<h1>ColorFace</h1>
+
+<h2>A face re-maker based on the piximilar API!</h2>
+
+<form name="newad" method="post" enctype="multipart/form-data" action="upload.php">
+<table>
+  <tr><td><input type="file" name="image"></td></tr>
+  <tr><td><input name="Submit" type="submit" value="Upload image"></td></tr>
+</table>
+</form>
+</body>
+</html>
